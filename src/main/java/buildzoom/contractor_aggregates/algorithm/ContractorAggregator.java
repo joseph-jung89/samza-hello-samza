e45f8c5fb4f005b541550ba61709a180de73c06d
Joseph <swjung@buildzoom.com>
2017-11-07 01:19:04 -0800

    let's finalize some basic things and see how things pan out

---
+++ src/main/java/buildzoom/contractor_aggregates/algorithm/ContractorAggregator.java
@@ -0,0 +1,16 @@
+
+
+
+public class ContractorAggregator {
+
+	public static HashMap<String, Integer> calculateAggregates(
+		HashMap<String, Integer> storedIntermediateMap, 
+		) {
+
+	}
+
+	public static HashMap<String, Integer> filterIntermediates(
+		HashMap<String, Integer> intermediateMap) {
+
+	}
+}
--- src/main/java/buildzoom/contractor_aggregates/task/ContractorAggregatesTask.java
+++ src/main/java/buildzoom/contractor_aggregates/task/ContractorAggregatesTask.java
@@ -1,9 +1,6 @@
 package buildzoom.contractor_aggregates.task;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 import org.apache.samza.config.Config;
 import org.apache.samza.metrics.Counter;
 import org.apache.samza.storage.kv.KeyValueStore;
@@ -15,32 +12,45 @@ import org.apache.samza.task.MessageCollector;
 import org.apache.samza.task.StreamTask;
 import org.apache.samza.task.TaskContext;
 import org.apache.samza.task.TaskCoordinator;
+import buildzoom.contractor_aggregates.models.ContractorAggregatesParser;
+import buildzoom.contractor_aggregates.algorithm.ContractorAggregator;
 
 // TODO: make this asynchronous, java threads and callbacks are pretty good, and there might be db call
 public class ContractorAggregatesTask implements StreamTask {
 
+	private KeyValueStore<String, HashMap<String, Integer>> store;
+	private final String KV_STORE_NAME = "aggregates-db";
 	private final SystemStream OUTPUT_STREAM = new SystemStream("kafka", "contractor-permit-aggregated");
+
+	public void init(Config config, TaskContext context) {
+		this.store = (KeyValueStore<String, HashMap<String, Integer>>) context.getStore(KV_STORE_NAME);
+	}
 
 	public void process(IncomingMessageEnvelope envelope, MessageCollector collector, TaskCoordinator coordinator) {
 		// TODO: define key-value store and start implementing stuff
-		int permitId = envelope.getKey();
-		// TODO: understand how this would work
-		Object contractor_permit_aggregate = envelope.getMessage();
+		String permitId = (String) envelope.getKey();
+		// TODO: understand how this would work, probably JSON?
+		Object contractorPermitMatch = envelope.getMessage();
+		contractorPermitMatch = ContractorAggregatesParser.parseMessage(contractorPermitMatch);
+		HashMap<String, Integer> updatedContractorAggregatesForKVStore = ContractorAggregator.calculateAggregates(store.get(permitId), contractorPermitMatch);
+		// state update for the key-value store
+		store.put(permitId, updatedContractorAggregatesForKVStore);
 
-
-
-		// after done processing
+		// after done processing, filter out the intermediates
+		HashMap<String, Integer> outputContractorAggregates = ContractorAggregator.filterIntermediates(updatedContractorAggregatesForKVStore)
 		OutgoingMessageEnvelope outgoingMessage = new OutgoingMessageEnvelope(
 														OUTPUT_STREAM,
 														keySerializerName,
 														messageSerializerName,
-														partitionKey,
-														permit_id,
-														aggregates
+														partitionKey, // maybe not include?
+														permitId,
+														updatedContractorAggregates
 													);
 		// fill in outgoing message details
-		outgoingMessage.setKey();
-		outgoingMessage.setMessage();
+		outgoingMessage.setKey(permitId);
+		outgoingMessage.setMessage(updatedContractorAggregates); // JSON casting?
 		collector.send(outgoingMessage);
+
+		// and also import Redis right?
 	}
 }
